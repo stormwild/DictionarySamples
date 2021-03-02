eedde2f705fe7306e802514fe92d95a35dbab0db
Alexander.Torrijos <alexander.torrijos@e-hps.com>
2021-03-02 21:33:50 +0800

    Initial Commit

---
+++ DictionarySamplesConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DictionarySamplesConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------------------");
+
+            var colors = new Dictionary<string, string>();
+
+            colors["warm"] = "Red";
+
+            Console.WriteLine($"A warm color is {colors.Values.FirstOrDefault()}");
+
+            var codes = new Dictionary<decimal, decimal>();
+
+            codes[1] = 1;
+
+            Console.WriteLine($"Code 1 has a value of {codes[1]}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
